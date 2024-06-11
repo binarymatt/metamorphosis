9df2991a6519c0bf56dbc0e0bd732338bc8772fe
Matt George <mgeorge@hashicorp.com>
2024-06-11 08:59:45 -0600

    adding more tests around config and context

--- context.go
+++ context.go
@@ -5,24 +5,31 @@ import (
 	"log/slog"
 )
 
-type ClientContextKey struct{}
-type LoggerContextKey struct{}
+type clientContextKey struct{}
+type loggerContextKey struct{}
 
+// LOgerWithContext adds the given logger to the returned Context
 func LoggerWithContext(ctx context.Context, logger *slog.Logger) context.Context {
-	return context.WithValue(ctx, LoggerContextKey{}, logger)
+	return context.WithValue(ctx, loggerContextKey{}, logger)
 }
+
+// LoggerFromContext retrieves a logger from the passed in Context or returns the default slog.Logger
 func LoggerFromContext(ctx context.Context) *slog.Logger {
-	logger, ok := ctx.Value(LoggerContextKey{}).(*slog.Logger)
+	logger, ok := ctx.Value(loggerContextKey{}).(*slog.Logger)
 	if !ok {
 		return slog.Default()
 	}
 	return logger
 }
+
+// ClientWithContext adds the given client to the returned Context
 func ClientWithContext(ctx context.Context, client *Client) context.Context {
-	return context.WithValue(ctx, ClientContextKey{}, client)
+	return context.WithValue(ctx, clientContextKey{}, client)
 }
+
+// ClientFromContext retrieves a Client from Context, if it exists in the given context.
 func ClientFromContext(ctx context.Context) *Client {
-	api, ok := ctx.Value(ClientContextKey{}).(*Client)
+	api, ok := ctx.Value(clientContextKey{}).(*Client)
 	if !ok {
 		return nil
 	}
